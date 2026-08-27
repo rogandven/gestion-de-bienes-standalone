96545afb804c089199884b2c7e82b1191e291330
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2026-08-26 22:29:07 -0400

    add: added PHP hello world (temporary)

---
+++ frontend/php/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Hola Mundo</title>
+    </head>
+    <body>
+        <div>
+        <?php
+            echo "<p>La fecha es " . date("Y-m-d") . "</p>";
+        ?>
+        </div>
+    </body>
+</html>
