fecd48efdaa5e41a0ac95cd55fd48ef24216c8a7
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2026-09-08 02:20:26 -0300

    add: add material service

---
+++ backend/php/service/materialService.php
@@ -0,0 +1,42 @@
+<?php
+    if (!defined('IS_PUBLIC')) {
+        header("Location: /");
+        die();
+    }
+
+    require __DIR__ . '/../classes/SQLHelper.php';   
+    
+    abstract class MaterialService {
+        public static function getMateriales($conn, $bodegaId) {
+            $response = NULL;
+
+            try {
+                $bodegaId = (int)$bodegaId;
+
+                $materiales = SQLHelper::fetchQuery($conn, "SELECT 
+                    m.ano_pdp, 
+                    m.bodega,
+                    m.nombre_material,
+                    m.sbo_stock_disponible,
+                    m.ume_descripcion,
+                    m.ume_codigo,
+                    m.sbo_valor,
+                    m.fla_correlativo,
+                    m.rub_correlativo,
+                    m.tma_correlativo,
+                    m.mte_correlativo,
+                    m.dme_correlativo
+                FROM material m WHERE m.bodega = " . $bodegaId . ";");
+
+                if ($materiales && is_array($materiales)) {
+                    $response = new ServiceResponse(200, "Material encontrado con éxito", $materiales);   
+                } else throw new Exception("Los materiales no son un arreglo");
+            } catch (Exception $e) {
+                error_log(json_encode($e), 0);
+                $response = new ServiceResponse(500, "Error interno del servidor", NULL); 
+            }
+
+            return $response;
+        }
+    }    
+?>
