dc16c08d249ad36da87cad0c5cc5fac2f17225aa
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2026-09-09 20:15:12 -0300

    fix: fixed CampusPicker syntax to prevent crashes

--- frontend/node/src/components/CampusPicker.jsx
+++ frontend/node/src/components/CampusPicker.jsx
@@ -2,14 +2,11 @@ function CampusPicker(campuses) {
   return (
     <>
         <select>
-            {campuses.map((campus, index) => {
+            {Array.isArray(campuses) && campuses.map((campus, index) => {
                 return <option value={campus?.cmp_codigo || index} key={"campus-" + index}>
                   {campus?.cmp_nombre || "[CAMPUS DESCONOCIDO]"}
                 </option>
             })}
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
         </select>
 
     </>
